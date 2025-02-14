a1bce8e4519cb9c55cd1ed11781fdbaa8fed32d0
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-02-14 13:15:35 +0100

    Add .gitignore and remove build artifacts from git

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DGK_29rJ.js","_app/immutable/chunks/C0I3Lh4A.js","_app/immutable/chunks/BiknXCUn.js","_app/immutable/chunks/DWh2R7JZ.js"];
-export const stylesheets = ["_app/immutable/assets/2.B0OixwC9.css"];
-export const fonts = [];
--- build/_app/immutable/entry/start.gNWKHEdr.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/DAAs-P4G.js";export{t as start};
